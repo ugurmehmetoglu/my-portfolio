27fb4ad77f636f337d1b30f24b716889cdce90e3
ugurmehmetoglu <ugurmehmetoglu@Ugurs-Air.lan>
2022-08-16 17:05:52 -0700

    Adds server

---
+++ server/config.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+    sendgrid_api: process.env.SENDGRID_API,
+    port: process.env.PORT
+}
---
+++ server/index.js
@@ -0,0 +1,76 @@
+const dotenv = require('dotenv');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mailer = require('nodemailer');
+const { port } = require('./config');
+const path = require('path');
+const router = express.Router();
+const bodyParser = require('body-parser')
+dotenv.config();
+app.use(bodyParser.json())
+app.use(cors("*"))
+
+
+
+// app.post('/contact', (req, res) => {
+//     console.log("req body",req.body)
+//     const{name, email, message} = req.body
+//     transporter.sendMail({
+//         to:'ugurmehmetoglu1903@gmail.com',
+//         from: email,
+//         html:`<h3>${name}</h3>
+//         <p>${message}</p>`
+
+//     }).then(res => {
+//         res.json({res})
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// })
+
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+})
+app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const transporter = mailer.createTransport({
+  service: 'gmail',
+  port: process.env.PORT,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  },
+})
+transporter.verify((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Ready To Send')
+  }
+})
+
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mail = {
+    from: name,
+    to: 'ugurmehmetoglu1903@gmail.com',
+    subject: 'Contact Form Submission',
+    html: `<p> Name: ${name} </p><p> Email: ${email}</p><p> Message: ${message}</p>`
+  }
+  transporter.sendMail(mail, (error, data) => {
+    if (error) {
+      res.json({ status: 'ERROR' })
+    } else {
+      res.json({ status: 'Message Sent' })
+    }
+  })
+})
