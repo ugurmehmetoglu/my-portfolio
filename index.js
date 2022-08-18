6a4b679a74351569deb533287f6782836b8518b0
ugurmehmetoglu <ugurmehmetoglu@Ugurs-Air.lan>
2022-08-17 19:03:50 -0700

    Updates file path

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.use(cors("*"))
 // })
 
 
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
 })
 app.use('/', router)
 
