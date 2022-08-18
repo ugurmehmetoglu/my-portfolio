939e630c317a7ca718423dd14e8be110204dac5f
ugurmehmetoglu <ugurmehmetoglu@Ugurs-Air.lan>
2022-08-17 17:46:23 -0700

    Updates folder structures

--- server/index.js
+++ server/index.js
@@ -30,10 +30,10 @@ app.use(cors("*"))
 // })
 
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
 app.use('/', router)
 
