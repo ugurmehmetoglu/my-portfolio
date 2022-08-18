9e5a5dc50635a8cfe9923e11d8231a3cd51d1a8c
ugurmehmetoglu <ugurmehmetoglu@Ugurs-Air.lan>
2022-08-17 19:08:23 -0700

    build

---
+++ client/build/precache-manifest.acefa70b318b4b184b6c4195b7fbcca2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "02efac65b62dcea78341",
+    "url": "/static/css/main.fc30bd94.chunk.css"
+  },
+  {
+    "revision": "02efac65b62dcea78341",
+    "url": "/static/js/main.02efac65.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "bcb3a91844fa8b8edcb9",
+    "url": "/static/js/2.bcb3a918.chunk.js"
+  },
+  {
+    "revision": "6911db604fedf1b3b856",
+    "url": "/static/js/runtime~main.6911db60.js"
+  },
+  {
+    "revision": "2eb00c562dcfd989653c4b62e115bdbc",
+    "url": "/static/media/resume1.2eb00c56.pdf"
+  },
+  {
+    "revision": "f6cae32f0e45a24fb076f0d76c6e7d03",
+    "url": "/static/media/IMG_3918.f6cae32f.jpg"
+  },
+  {
+    "revision": "09ab8829ecab09040ccae31cd75bde5b",
+    "url": "/static/media/resume.09ab8829.pdf"
+  },
+  {
+    "revision": "e4dd2ab4a7e4bd7bc87189dc5d8e5371",
+    "url": "/index.html"
+  }
+];
